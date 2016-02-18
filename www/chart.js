2c72098563a1c1318481be3a2c5ba07cb9c743a2
SuperBonny <bonora.matteo@gmail.com>
2016-02-18 09:10:23 +0100

    Further code cleaning

--- www/chart.js
+++ www/chart.js
@@ -7,36 +7,35 @@ var values;
 
 google.charts.setOnLoadCallback(setInterval(getData(dataPath, function(data) { values = $.csv.toArrays(data); drawChart(); }), 10000));
 
-      function drawChart() {
+function drawChart() {
 	var data = new google.visualization.DataTable();
 	
 
 	$.each(values, function (i, row) {
 		data.addColumn(new Date(row[0]));
 		data.addRow(parseFloat(row[1]));
-
 	});
         
-        var options = {
-          title: 'Company Performance',
-          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-          vAxis: {minValue: 0}
-        };
+    var options = {
+		title: 'Company Performance',
+		hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+		vAxis: {minValue: 0}
+	};
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chartArea'));
-        chart.draw(data, options);
-      }
+	var chart = new google.visualization.AreaChart(document.getElementById('chartArea'));
+	chart.draw(data, options);
+}
 
 
 function getData (url, action) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url, true);
-  req.onreadystatechange = function() {
-    if(req.readyState == 4) {
-      if(req.status == 200) {
-        action(req.responseText);
-      }
-    }
-  };
-  req.send(null);
+	var req = new XMLHttpRequest();
+	req.open("GET", url, true);
+	req.onreadystatechange = function() {
+		if(req.readyState == 4) {
+			if(req.status == 200) {
+				action(req.responseText);
+			}
+		}
+	};
+	req.send(null);
 }
