4286e7aafa8e99e8e042dc2876bb1e1b7b6c7ac3
SuperBonny <bonora.matteo@gmail.com>
2016-02-19 06:51:07 +0100

    Initial charts auto-update function

--- www/chart.js
+++ www/chart.js
@@ -1,30 +1,48 @@
-var dataPath = "log.csv"
+var dataPath = "log.csv";
 
-google.charts.setOnLoadCallback( function() {
+google.charts.setOnLoadCallback(function() { parse(); setInterval(parse, 60000) }); 
+
+function parse() {
 	getData(dataPath, function(a) { 
 		drawChart($.csv.toArrays(a));
 	})
-});
-
-
+}
 
 function drawChart(data) {
-	var table = new google.visualization.DataTable();
+	drawHistory(data);
+	drawCurrent(data);
+}
 
+function drawHistory(data) {
+	var table = new google.visualization.DataTable();
 	table.addColumn('datetime', 'Time');
 	table.addColumn('number', 'Level');
 
 	$.each(data, function (i, row) {
 		table.addRow([new Date(row[0]), parseFloat(row[1])]);
 	});
-        
-    var options = {
-		title: 'Water Level',
+
+	var options = {
+		title: 'Level History',
 		hAxis: {title: 'Date',  titleTextStyle: {color: '#333'}},
-		vAxis: {minValue: 0}
+		vAxis: {minValue: 0, maxValue: 30}
 	};
 
-	var chart = new google.visualization.AreaChart(document.getElementById('chartArea'));
+	var chart = new google.visualization.AreaChart(document.getElementById('historyChart'));
+	chart.draw(table, options);
+}
+
+function drawCurrent(data) {
+	var table =google.visualization.arrayToDataTable([
+          ['Dinosaur', 'Length'],
+          ['Acrocanthosaurus (top-spined lizard)', data[data.length-1,1]]]);
+
+        var options = {
+          title: 'Water Level [cm]',
+          legend: { position: 'none' },
+	  vAxis: {maxValue: 30, minValue:0}
+        }; 
+	var chart = new google.visualization.Histogram(document.getElementById('currentChart'));
 	chart.draw(table, options);
 }
 
@@ -41,5 +59,3 @@ function getData (url, action) {
 	};
 	req.send(null);
 }
-
-
