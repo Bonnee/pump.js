08e43c7b0179157f410fe8deb05afd11def1720b
SuperBonny <bonora.matteo@gmail.com>
2016-04-05 13:04:59 +0200

    Change port number

--- pump/www/node/pump.js
+++ pump/www/node/pump.js
@@ -1,7 +1,7 @@
 process.stdout.write("Starting server...");
 
 var cn =  require('./connection.js');
-var client = new cn('ws://192.168.1.120:10611');
+var client = new cn('ws://192.168.1.120:11111');
 
 var sr = require('./serial.js');
 var arduino = new sr('/dev/ttyATH0');
