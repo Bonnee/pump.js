836112f7a7ffec7223238ccb40fbec7af2f5c0f2
SuperBonny <bonora.matteo@gmail.com>
2016-04-07 14:24:55 +0200

    Fixes to keep compatibility to OsO.js

--- pump/www/node/pump.js
+++ pump/www/node/pump.js
@@ -6,25 +6,25 @@ var client = new cn('ws://192.168.1.13:11111');
 var sr = require('./serial.js');
 var arduino = new sr('/dev/ttyATH0');
 
-arduino.on('open', function() {
+arduino.on('open', function () {
     console.log('Connected to Arduino');
     console.log('Running ', arduino.firmware);
     arduino.pinMode(13, arduino.HIGH);
 });
 
-client.on('open', function() {
+client.on('open', function () {
     console.log('Connected to iot server');
     // Send MAC address for identification
-    require('getmac').getMac(function(err, mac) {
+    require('getmac').getMac(function (err, mac) {
         if (err) throw err
         client.send('hello', mac);
     });
 });
 
-client.on('connection', function(data) {
+client.on('connection', function (data) {
     data = JSON.parse(data);
     if (data.id == 'who') {
-        require('fs').readFile('manifest.json', function(err, data) {
+        require('fs').readFile('manifest.json', function (err, data) {
             ws.send(data);
         });
     }
