477f15b18f1b89e361e2fe81b52fe115138fb954
Matteo Bonora <bonora.matteo@gmail.com>
2016-04-17 17:26:34 +0200

    Remove serialport dependencies

--- pump/node/serial.js
+++ pump/node/serial.js
@@ -1,47 +1,10 @@
-var SerialPort = require("serialport").SerialPort
+//var SerialPort = require("serialport").SerialPort
 
-var ev = require('events').EventEmitter;
-var util = require("util");
+var ev = require( 'events' ).EventEmitter;
+var util = require( "util" );
 
-this.serial = function(port, baud) {
-    var self = this;
-    
-    var arduino = new SerialPort(port, {
-        baudrate: baud,
-        dataBits: 8,
-        parity: 'none',
-        stopBits: 1,
-        flowControl: false
-    }, false);
+this.serial = function( port, baud ) {}
 
-    this.open = function() {
-        arduino.open();
-    }
-
-    arduino.on('open', function() {
-        self.emit('open');
-
-        arduino.write('prova lel\n');
-
-        arduino.on('data', function(data) {
-            console.log(data);
-            console.log(data.toString('UTF-8'));
-        });
-
-        arduino.on('disconnect', function() {
-            self.emit('disconnect');
-        })
-
-        arduino.on('close', function() {
-            console.log('The port has closed.');
-        })
-
-        arduino.on('error', function(er) {
-            console.log(error);
-        });
-    });
-}
-
-util.inherits(this.serial, ev);
-ev.call(this);
+util.inherits( this.serial, ev );
+ev.call( this );
 module.exports = this.serial;
--- pump/pump.ino
+++ pump/pump.ino
@@ -1,4 +1,7 @@
-//#include <FileIO.h>
+#include <Arduino.h>
+#include <Process.h>
+
+Process nodejs;
 
 #define ON        0
 #define OFF       1
@@ -31,110 +34,112 @@ float liv;
 
 int index = 1;
 
-const int STOREFREQ = 30; // The amount of readings cycles before saving the value to SD.
+const int STOREFREQ = 1; // The amount of readings cycles before saving the value to SD.
 int storeIndex = 1;
 
 unsigned long p = 0;
 
 void setup() {
-  pinMode(13, OUTPUT); //Loading led
-  digitalWrite(13, HIGH);
+        pinMode(13, OUTPUT); //Loading led
+        digitalWrite(13, HIGH);
 
-  for (int i = 0; i < sizeof(relay); i++) {
-    pinMode(relay[i], OUTPUT);
-    digitalWrite(relay[i], OFF);
-  }
-
-  //Serial.begin(9600);     // Serial log interface over USB
-  /*printlog("Pump Control System v0.5");
-  printlog("Initializing Bridge...");*/
-  
-  SERIAL_PORT_HARDWARE.begin(250000); // Replacement for the bridge
-  
-  do {
-     while (SERIAL_PORT_HARDWARE.available() > 0) {
-        SERIAL_PORT_HARDWARE.read();
+        for (int i = 0; i < sizeof(relay); i++) {
+                pinMode(relay[i], OUTPUT);
+                digitalWrite(relay[i], OFF);
         }
-    
-    delay(1000);
-  } while (SERIAL_PORT_HARDWARE.available()>0);
-  digitalWrite(13, LOW);
 
-  p = millis();
+        Bridge.begin(); // Initialize the Bridge
+        Serial.begin(9600); // Initialize the Serial
+        while (!Serial);
+
+        printlog("Starting...");
+        nodejs.runShellCommandAsynchronously("node /mnt/sda1/arduino/pump/node/pump.js");
+        printlog("Started process");
+
+        digitalWrite(13, LOW);
+
+        p = millis();
 }
 
-void loop() {  
-  SERIAL_PORT_HARDWARE.write('hello');
-  unsigned long c = millis();
-  if ((long)(c - p) >= 0) {
-    p += wait;
+void loop() {
+        unsigned long c = millis();
+        if ((long)(c - p) >= 0) {
+                p += wait;
 
-    printlog("Reading partial level [" + String(index) + " of " + String(SAMPLES) + "]");
-    reading[index - 1] = getLevel(true);
+                printlog("Reading partial level [" + String(index) + " of " + String(SAMPLES) + "]");
+                reading[index - 1] = getLevel(true);
 
-    if (index == SAMPLES) {
-      liv = 0;
-      // avg calculation
-      for (int i = 0; i < SAMPLES; i++)
-        liv += reading[i];
-      liv = mapFloat(liv / SAMPLES, vMin, vMax, livMax, livMin);
+                if (index == SAMPLES) {
+                        liv = 0;
+                        // avg calculation
+                        for (int i = 0; i < SAMPLES; i++)
+                                liv += reading[i];
+                        liv = mapFloat(liv / SAMPLES, vMin, vMax, livMax, livMin);
 
-      SERIAL_PORT_HARDWARE.println(liv);
+                        if (nodejs.running()) {
+                                printlog("Sending to node....");
+                                nodejs.write('{ "level":' + liv + ' }');
+                        }
 
-      checkThresold();
-      index = 1;
-    } else {
-      index++;
-    }
-  }
-  delay(0);
+                        checkThresold();
+                        index = 1;
+                } else {
+                        index++;
+                }
+        }
+
+        while (nodejs.available()) {
+                Serial.write(nodejs.read());
+        }
+
+        delay(0);
 }
 
 void checkThresold() {
-  // relay 1
-  if (liv <= 50)
-    digitalWrite(relay[0], ON);
-  else if (liv >= 70)
-    digitalWrite(relay[0], OFF);
+        // relay 1
+        if (liv <= 50)
+                digitalWrite(relay[0], ON);
+        else if (liv >= 70)
+                digitalWrite(relay[0], OFF);
 
-  // relay 2
-  if (liv <= 45)
-    digitalWrite(relay[1], ON);
-  else if (liv >= 64)
-    digitalWrite(relay[1], OFF);
+        // relay 2
+        if (liv <= 45)
+                digitalWrite(relay[1], ON);
+        else if (liv >= 64)
+                digitalWrite(relay[1], OFF);
 
-  // alarm
-  if (liv <= 5)
-    digitalWrite(relay[2], ON);
-  else if (liv >= 7)
-    digitalWrite(relay[2], OFF);
+        // alarm
+        if (liv <= 5)
+                digitalWrite(relay[2], ON);
+        else if (liv >= 7)
+                digitalWrite(relay[2], OFF);
 
-  bool a = false;
-  for (int i = 0; i < ALARMS; i++) {
-    if (digitalRead(alarm[i]) == HIGH) {
-      a = true;
-    }
-    // Code to trigger the notification method in Linux using the Arduino bridge
-  }
-  if (a)
-    digitalWrite(13, HIGH);
-  else
-    digitalWrite(13, LOW);
+        bool a = false;
+        for (int i = 0; i < ALARMS; i++) {
+                if (digitalRead(alarm[i]) == HIGH) {
+                        a = true;
+                }
+                // Code to trigger the notification method in Linux using the Arduino bridge
+        }
+        if (a)
+                digitalWrite(13, HIGH);
+        else
+                digitalWrite(13, LOW);
 }
 
 float getLevel(bool raw) {
-  if (raw)
-    return analogRead(SENSOR);
-  else
-    return mapFloat(analogRead(SENSOR), vMin, vMax, livMax, livMin);
+        if (raw)
+                return analogRead(SENSOR);
+        else
+                return mapFloat(analogRead(SENSOR), vMin, vMax, livMax, livMin);
 }
 
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void printlog(String message) {
-  if (Serial)
-    Serial.println(message);
+        if (Serial)
+                Serial.println(message);
 }
