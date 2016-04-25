27ed6e3f097fae97e445affb7c73dd77507713eb
Matteo Bonora <bonora.matteo@gmail.com>
2016-04-25 14:32:00 +0200

    Switch to Socket.io from ws

--- pump/node/connection.js
+++ pump/node/connection.js
@@ -1,24 +1,33 @@
-var ws = require('ws');
 var ev = require('events').EventEmitter;
 var util = require("util");
 
-this.client = function (url) {
-    var self = this;
-    var wsc = new ws(url);
+this.Client = function(addr) {
+	var self = this;
+	var io = require('socket.io-client')(addr);
 
-    wsc.on('open', function open() {
-        self.emit('open');
-    });
+	io.on('connect', function open() {
+		console.log('mark');
+		self.emit('connect');
+	});
 
-    wsc.on('message', function (data) {
-        self.emit('message', data);
-    });
+	io.on('pair', function(data) {
+		self.emit('pair', data);
+	});
 
-    this.send = function(id, data) {
-      wsc.send(JSON.stringify({ "id": id, "data": data }));
-    }
+	io.on('error', function(data) {
+		self.emit('hello', data);
+	})
+
+	io.on('message', function(data) {
+		self.emit('message', data);
+	});
+
+	this.emit = function(id, data) {
+		io.emit(id, data);
+	}
 }
 
-util.inherits(this.client, ev);
+//util.inherits(this.client, ev);
+util.inherits(this.Client, ev);
 ev.call(this);
-module.exports = this.client;
+module.exports = this;
--- pump/node/pump.js
+++ pump/node/pump.js
@@ -1,8 +1,8 @@
-process.stdout.write("Starting server...");
+process.stdout.write("Starting...");
 
-var cn = require('./connection.js');
-var client = new cn('ws://192.168.1.4:11111');
+var address = 'http://192.168.1.4:11111';
 
+process.stdout.write('loading bridge...');
 var bridge = require('./bridge.js');
 var arduino = new bridge();
 
@@ -11,37 +11,53 @@ arduino.on('data', function(data) {
 	console.log('Arduino: ' + data);
 	data = JSON.parse(data);
 	if (data.id == 'levels') {
-		client.send('log', JSON.stringify({
+		io.emit('log', {
 			type: data.id,
 			timestamp: new Date().toISOString(),
 			level: data.data
-		}));
+		});
 	}
 });
 
 // OsO connection code
-client.on('open', function() {
+process.stdout.write('loading socket...');
+var io = require('socket.io-client')(address);
+
+io.once('connect', function open() {
 	console.log('Connected to Ohm sweet Ohm');
 	// Send MAC address for identification
 	getMac(function(mac) {
-		client.send('hello', mac);
+		io.emit('hello', mac);
 	});
 });
 
-client.on('message', function(data) {
-
-	data = JSON.parse(data);
-
-	console.log('Received: ' + data.id + ", " + data.data);
-
-	if (data.id == 'who') {
-		var manifest;
-		require('fs').readFile('/mnt/sda1/arduino/node/manifest.json', function(err, manifest) {
-			manifest = JSON.parse(manifest);
-			client.send('who', JSON.stringify(manifest));
-		});
-	} else {}
+io.once('pair', function(data) {
+	var manifest;
+	require('fs').readFile('/mnt/sda1/arduino/node/manifest.json', function(err, manifest) {
+		//manifest = JSON.parse(manifest);
+		io.emit('cpair', JSON.parse(manifest));
+	});
 });
+
+io.once('hello', function() {
+	console.log('Successfully associated to server');
+})
+
+io.on('error', function(data) {
+	console.log(data);
+})
+
+io.on('message', function(data) {
+	console.log('Received: ' + data)
+});
+
+io.on('disconnect', function() {
+	console.log('Disconnected from server.');
+})
+
+io.on('reconnect', function() {
+	console.log('Connected to server.');
+})
 
 function getMac(back) {
 	require('getmac').getMac(function(err, mac) {
@@ -50,4 +66,4 @@ function getMac(back) {
 	});
 }
 
-console.log('started.');
+console.log('done.');
