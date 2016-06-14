62aa1046297e679d50aa784a146757a6861dcf25
Matteo Bonora <bonora.matteo@gmail.com>
2016-06-15 00:36:55 +0200

    Enhanced frontend

--- pump/code/frontend/script.js
+++ pump/code/frontend/script.js
@@ -4,48 +4,51 @@ function main(data, $scope) {
 	data = JSON.parse(JSON.stringify(data));
 	$scope.data = data;
 
+	var levelChart;
 	$.getScript("//dygraphs.com/1.1.1/dygraph-combined.js", function() {
-
-		for (var i = 0; i < $scope.data.data.level.length; i++) {
-			$scope.data.data.level[i][0] = new Date($scope.data.data.level[i][0]);
-		}
-
-		var levelChart = new Dygraph(document.getElementById("levelChart"), $scope.data.data.level, {
+		levelChart = new Dygraph(document.getElementById("levelChart"), $scope.data.data.level, {
 			labels: ['Time', 'Level'],
 			valueRange: [108, 28],
 			legend: 'always',
 			animatedZooms: true,
+			color: "#337ab7"
 		});
 
-		levelChart.ready(function() {
-			var ann = [];
-
-			if ($scope.data.data.pump1) {
-				$scope.data.data.pump1.forEach(function(value, key) {
-					var state = ((value[1]) ? "on" : "off");
-					ann.push(getAnn('Level', Date.parse(value[0]), "1", "pump1 " + state, "1st Pump " + state));
-				});
-			}
-
-			if ($scope.data.data.pump2) {
-				$scope.data.data.pump2.forEach(function(value, key) {
-					var state = ((value[1]) ? "on" : "off");
-					ann.push(getAnn('Level', Date.parse(value[0]), "2", "pump2 " + state, "2nd Pump " + state));
-				});
-			}
-			console.log(ann);
-			levelChart.setAnnotations(ann);
-
-			function getAnn(series, x, shortText, cssClass, text) {
-				return {
-					series: series,
-					x: x,
-					shortText: shortText,
-					cssClass: classClass,
-					text: text
-				}
-			}
-
-		});
+		levelChart.ready(annotations);
 	});
+
+	for (var i = 0; i < $scope.data.data.level.length; i++) {
+		$scope.data.data.level[i][0] = new Date($scope.data.data.level[i][0]);
+	}
+
+
+	function annotations() {
+		var ann = [];
+
+		if ($scope.data.data.pump1) {
+			$scope.data.data.pump1.forEach(function(value, key) {
+				var state = value[1] == 1 ? "on" : "off";
+				ann.push(getAnn('Level', Date.parse(value[0]), "1", "pump1 " + state, "1st Pump " + state));
+			});
+		}
+
+		if ($scope.data.data.pump2) {
+			$scope.data.data.pump2.forEach(function(value, key) {
+				var state = value[1] == 1 ? "on" : "off";
+				ann.push(getAnn('Level', Date.parse(value[0]), "2", "pump2 " + state, "2nd Pump " + state));
+			});
+		}
+		console.log(ann);
+		levelChart.setAnnotations(ann);
+
+		function getAnn(series, x, shortText, cssClass, text) {
+			return {
+				series: series,
+				x: x,
+				shortText: shortText,
+				cssClass: cssClass,
+				text: text
+			}
+		}
+	}
 }
